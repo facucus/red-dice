21b61d9b534a9055ac8eec465b9fa012baf1887b
Maximiliano Arias <fmechetti@gmail.com>
2017-02-17 09:34:44 -0300

    Change user

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react'
 const Greetings = (props) => {
   return (
     <div className="jumbotron">
-      <h1>Hello from React!!!</h1>
+      <h1>Hello from React!</h1>
     </div>
   )
 }
